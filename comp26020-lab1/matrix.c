7f699523447bef00de7e5f326844f9305e45d4bb
AlyScript <adamaly93@gmail.com>
2024-09-30 16:27:07 +0100

    Everything upto transposition.

---
+++ comp26020-lab1/matrix.c
@@ -0,0 +1,186 @@
+// gcc -D__TEST_NAME__=allocate basic-test-suite.c matrix.c unity.c -o allocate_test
+// gcc basic-test-suite.c matrix.c unity.c -o all_tests
+
+#include "matrix.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h> /* for ENOSYS */
+
+void print_matrix(matrix_t *m) {
+    printf("\n");
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->columns; j++) {
+            printf("%d ", m->content[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int matrix_allocate(matrix_t *m, int rows, int columns) {
+    m->rows = rows;
+    m->columns = columns;
+
+    // content is a pointer to each containing a reference to each row, so we allocate rows * size of an int pointer because we want rows number of pointers to int arrays.
+    m->content = (int **) malloc(rows * sizeof(int *));
+    if (m->content == NULL) {
+        return -1;
+    }
+
+    /* now we allocate memory for each row
+     * each content[i] is the same as *(content + i) which is a pointer to an array of integers
+     * therefore we allocate columns * size of an int for each row, and each content[i] points to this 
+    */ 
+    for (int i = 0; i < rows; i++) {
+        m->content[i] = (int *) malloc(columns * sizeof(int));
+        if (m->content[i] == NULL) {
+            // Handle memory allocation failure
+            // Free previously allocated memory
+            for (int j = 0; j < i; j++) {
+                free(m->content[j]);
+            }
+            free(m->content);
+            return -ENOMEM;
+        }
+    }
+
+    // some sanity checks
+    // printf("Rows: %d, Columns: %d\n", rows, columns);
+    // printf("Size of m (the struct) is: %lu\n", sizeof(*m));
+    // printf("Size of the pointer to m is: %lu\n", sizeof(m));
+    // printf("Size of each content pointer is: %lu -- We have 10 of these, one for each row.\n", sizeof(m->content[1]));
+    // printf("Size of the what the content pointer points to is: %lu\n", sizeof(*((m->content[0]) + 1)));
+    return 0;
+}
+
+// free the memory allocated for the matrix
+void matrix_free(matrix_t *m) {
+    // first we free each row
+    for (int i = 0; i < m->rows; i++) {
+        free(m->content[i]);
+    }
+    // then free the pointer to the rows
+    free(m->content);
+    m->content = NULL;
+    m->rows = 0;
+    m->columns = 0;
+}
+
+void matrix_init_n(matrix_t *m, int n) {
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->columns; j++) {
+            m->content[i][j] = n;
+        }
+    }
+    // print_matrix(m);
+}
+
+void matrix_init_zeros(matrix_t *m) {
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->columns; j++) {
+            m->content[i][j] = 0;
+        }
+    }
+    // print_matrix(m);
+}
+
+int matrix_init_identity(matrix_t *m){
+    // identity matrix must be square
+    if (m->rows != m->columns) {
+        return -1;
+    }
+
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->columns; j++) {
+            if (i == j) {
+               m->content[i][j] = 1;
+            } else {
+                m->content[i][j] = 0;
+            }
+        }
+    }
+    // print_matrix(m);
+    return 0;
+}
+
+int matrix_init_rand(matrix_t *m, int val_min, int val_max) {
+    if (val_min > val_max) {
+        return -1;
+    }
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->columns; j++) {
+            m->content[i][j] = rand() % (val_max - val_min + 1) + val_min;
+        }
+    }
+    // print_matrix(m);
+    return 0;
+}
+
+int matrix_equal(matrix_t *m1, matrix_t *m2) {
+    // initial check for same dimensions
+    if (m1->rows != m2->rows || m1->columns != m2->columns) {
+        return 0;
+    }
+
+    // check all cells
+    for (int i = 0; i < m1->rows; i++) {
+        for (int j = 0; j < m1->columns; j++) {
+            if (m1->content[i][j] != m2->content[i][j]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int matrix_sum(matrix_t *m1, matrix_t *m2, matrix_t *result) {
+    if (m1->rows != m2->rows || m1->columns != m2->columns) {
+        return -1;
+    }
+    matrix_allocate(result, m1->rows, m1->columns);
+    for (int i = 0; i < m1->rows; i++) {
+        for (int j = 0; j < m1->columns; j++) {
+            result->content[i][j] = m1->content[i][j] + m2->content[i][j];
+        }
+    }
+    // print_matrix(result);
+    return 0;
+}
+
+int matrix_scalar_product(matrix_t *m, int scalar, matrix_t *result) {
+    matrix_allocate(result, m->rows, m->columns);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->columns; j++) {
+            result->content[i][j] = m->content[i][j] * scalar;
+        }
+    }
+    print_matrix(result);
+    return 0;
+}
+
+// calculate the transpose of a matrix
+int matrix_transposition(matrix_t *m, matrix_t *result) {
+    matrix_allocate(result, m->columns, m->rows);
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->columns; j++) {
+            result->content[j][i] = m->content[i][j];
+        }
+    }
+    // print_matrix(m);
+    // print_matrix(result);
+    return 0;
+}
+
+int matrix_product(matrix_t *m1, matrix_t *m2, matrix_t *result) {
+    /* implement the function here ... */
+    return -ENOSYS;
+}
+
+int matrix_dump_file(matrix_t *m, const char *output_file) {
+    /* implement the function here ... */
+    return -ENOSYS;
+}
+
+int matrix_allocate_and_init_file(matrix_t *m, const char *input_file) {
+    /* implement the function here ... */
+    return -ENOSYS;
+}
